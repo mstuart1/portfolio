0de55c0b4259d762af7fabee63673a001bf93dfc
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-18 09:55:06 -0400

    editing about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
           I enjoy translating complex data into elegant, accessible web tools and love working at the intersection of science, design, and technology.
         </p>
         <p>
-          My work blends strong technical architecture with human-centered design. Outside of tech, I love crocheting, gardening, and restoring old houses.
+          My work blends strong technical architecture with human-centered design. Outside of tech, I love crocheting, gardening, and restoring an old house.
         </p>
         <p>While I was a {" "}
           <Link
@@ -18,9 +18,9 @@ export default function AboutPage() {
           >
             molecular marine biologist
           </Link>
-          , I had to writem bash scripts, python scripts, and wrangle data in R in order to analyze massive amounts of DNA data that I stored in SQL relational databases.  Through this, I discovered a love of coding and found excuses to code up and automate as much of my life as possible.</p>
+          , I had to write bash scripts, python scripts, and wrangle data in R to analyze massive amounts of DNA data that I stored in SQL relational databases.  Through this, I discovered a love of coding and found excuses to code up and automate as much of my life as possible.</p>
 
-        <p> When the opportunity arose to jump from the lab to a tech role in 2020, I seized it. I was hired as a database administrator but within 3 months, I taught myself JavaScript, the ArgGIS SDK, and React and was taking on projects as a full-stack developer.</p>
+        <p> When the opportunity arose to jump from the lab to a tech role in 2020, I seized it. I was hired as a database administrator, but within 3 months, I taught myself JavaScript, the ArgGIS SDK, and React and was taking on projects as a full-stack developer.</p>
 
       <p> You can see from my long list of {" "}
           <Link
