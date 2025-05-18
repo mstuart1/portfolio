eff5f3c0512334e9947743db425dfdfaae336c6c
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-18 11:11:59 -0400

    update next config with basepath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
   images: { unoptimized: true },
+  basePath: isProd ? '/portfolio' : '',
 };
 
 export default nextConfig;
