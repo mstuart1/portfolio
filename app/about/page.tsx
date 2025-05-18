1805b15dc72aaca3256dba1ba6c051ef66def716
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-18 09:24:01 -0400

    add to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,11 @@ export default function AboutPage() {
         <p>
           My work blends strong technical architecture with human-centered design. Outside of tech, I love crocheting, gardening, and restoring old houses.
         </p>
+        <p>While I was a molecular marine biologist, I had to write bash scripts, python scripts, and wrangle data in R in order to analyze massive amounts of DNA data.  I maintained this data in SQL relational databases.  Through this, I discovered a love of coding and found excuses to code up and automate as much of my life as possible.</p>
+
+        <p> When the opportunity arose to jump from the lab to a tech role in 2020, I seized it. I was hired as a database administrator but within 3 months, I taught myself JavaScript, the ArgGIS SDK, and React and was taking on projects as a full-stack developer.</p>
+
+      <p> You can see from my long list of projects completed since then that I've been continuously learning and growing as a developer while handling multiple projects simultaneously.</p>
       </section>
     )
   }
