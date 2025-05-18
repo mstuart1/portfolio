5d86adeee451d0be946e58b3382a77a8a1041483
Michelle Stuart <michelle.stuart@rutgers.edu>
2025-05-18 09:33:36 -0400

    add links to projects page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function AboutPage() {
     return (
       <section className="space-y-4">
@@ -9,11 +11,11 @@ export default function AboutPage() {
         <p>
           My work blends strong technical architecture with human-centered design. Outside of tech, I love crocheting, gardening, and restoring old houses.
         </p>
-        <p>While I was a molecular marine biologist, I had to writem bash scripts, python scripts, and wrangle data in R in order to analyze massive amounts of DNA data that I stored in SQL relational databases.  Through this, I discovered a love of coding and found excuses to code up and automate as much of my life as possible.</p>
+        <p>While I was a <Link href='previous'>molecular marine biologist</Link>, I had to writem bash scripts, python scripts, and wrangle data in R in order to analyze massive amounts of DNA data that I stored in SQL relational databases.  Through this, I discovered a love of coding and found excuses to code up and automate as much of my life as possible.</p>
 
         <p> When the opportunity arose to jump from the lab to a tech role in 2020, I seized it. I was hired as a database administrator but within 3 months, I taught myself JavaScript, the ArgGIS SDK, and React and was taking on projects as a full-stack developer.</p>
 
-      <p> You can see from my long list of projects completed since then that I&apos;ve been continuously learning and growing as a developer while handling multiple projects simultaneously.</p>
+      <p> You can see from my long list of <Link href="/projects">projects</Link> completed since then that I&apos;ve been continuously learning and growing as a developer while handling multiple projects simultaneously.</p>
       </section>
     )
   }
